70aa94740274b4b3a2d6ea448b256c006633632a
byapy <105462085+byapy@users.noreply.github.com>
2025-02-19 16:50:49 -0600

    Pilares Terminados
    Solo a la espera de tener el modelo de Lilith y su arma

---
+++ Assets/Scenes/Nivel4/Pilar/DestruccionPilar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruccionPilar : MonoBehaviour
+{
+    public GameObject PilarIntacto;
+    public GameObject PilarDestruido;
+    public float VidaPilar;
+    public GameObject ParticulaDest;
+    void Start()
+    {
+        PilarDestruido.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (VidaPilar == 0)
+        {
+            Destruido();
+
+        };
+    }
+    void Destruido()
+    {
+        Debug.Log("Un pilar ha sido destruido");
+        PilarIntacto.SetActive(false);
+        PilarDestruido.SetActive(true);
+        ParticulaDest.SetActive(true);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.transform.tag == "Player"))//Remplazar esto por el arma de Lilith
+        {
+            Debug.Log("Se le hizo daño a un pilar");
+            VidaPilar = VidaPilar - 1;
+        }
+    }
+    
+}
