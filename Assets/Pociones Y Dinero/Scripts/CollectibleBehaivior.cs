c0224ccae7e395e83b190f457ae82d6bb5db78a4
N-raptor <2023-121184@intecap.edu.gt>
2025-04-20 23:00:12 -0600

    Nivel3
    Agregado nivel 3 y pociones y fichas para recoger con sonido

---
+++ Assets/Pociones Y Dinero/Scripts/CollectibleBehaivior.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleBehaivior : MonoBehaviour
+{
+
+    //public string ColorPocion;
+    public GameObject ParticulaRecolectado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            try
+            {
+                Instantiate(ParticulaRecolectado, other.transform.position, transform.rotation);
+            }
+            catch(System.Exception ex)
+            {
+                Debug.Log("error: " + ex.Message);
+            }
+            finally
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        
+       /*     switch(ColorPocion.ToLower())
+            {
+                case "azul":
+                    Debug.Log("Poción de defensa añadida.");
+                    Destroy(this.gameObject);
+                    break;
+
+                case "rojo":
+                    Debug.Log("Poción de vida añadida.");
+                    Destroy(this.gameObject);
+                    break;
+
+                case "verde":
+                    Debug.Log("Poción de ataque añadida.");
+                    Destroy(this.gameObject);
+                    break;
+
+                case "oro":
+                    Debug.Log("Encontraste 1 pieza de oro.");
+                    Destroy(this.gameObject);
+                    break;
+                case "cafe":
+                    Debug.Log("Encontraste una bolsa con 50 de oro.");
+                    Destroy(this.gameObject);
+                    break;
+
+            }
+
+        }*/
+    }
+
+
+
+}
