05c44f5db45010df83ec37916ff97d48ef370540
byapy <105462085+byapy@users.noreply.github.com>
2025-05-07 16:14:42 -0600

    Puerta de la Torre

--- Assets/Anciano/Scripts AncianoNPC/PlayerController.cs
+++ Assets/Anciano/Scripts AncianoNPC/PlayerController.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public int nivelProgreso = 1;
-   
+
+    public GameObject PuertaBloqueo;
+    public GameObject Fuego;
+
+    public void Start()
+    {
+        ActualizarPuerta();
+        Fuego.SetActive(false);
+    }
     public void AumentarProgreso()
     {
         nivelProgreso++;
         Debug.Log($"Progreso actualizado Nivel actual {nivelProgreso}");
+        ActualizarPuerta();
+    }
+    
+    private void ActualizarPuerta()
+    {
+        PuertaBloqueo.SetActive(nivelProgreso < 3); //Aqui mientras sea menor a 3, va estar false
+        Fuego.SetActive(nivelProgreso >= 3); //Lo mismo pero con true, en las fogatas que aparecen cuando esta abierto.
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (nivelProgreso < 3)
+            {
+                Debug.Log("Regresa cuando tengas todas las gemas");
+
+            }
+            
+        }
     }
 }
