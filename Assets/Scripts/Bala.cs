6a5ca15d83d7bbc1264bf340189935ff33975c10
N-raptor <2023-121184@intecap.edu.gt>
2025-05-23 20:41:11 -0600

    Nivel Final Updated
    Montañas agregadas para el nivel 3

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -26,40 +26,8 @@ public class Bala : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Instantiate(ParticulaExplocion, transform.position, Quaternion.identity);
+        Debug.Log("Pego en " + other.tag);
 
-
-        /*if (other.CompareTag("Enemy"))
-        {
-            Destroy(gameObject);
-            movimientoEnemigo.Instance.ReceivedDamage(BalaDamage);
-            
-        }
-        if (other.CompareTag("Enemy2"))
-        {
-            Destroy(gameObject);
-            movimientoEnemigo2.Instance.ReceivedDamage2(BalaDamage);
-
-        }
-        if (other.CompareTag("Jefe"))
-        {
-            Destroy(gameObject);
-            movimientoJefe.Instance.ReciveDanoJefe(BalaDamage);
-
-        }
-        if (other.CompareTag("Lilith"))
-        {
-         
-            JefaLilith.Instance.RecibirDaño(StatsPlayer.Instance.CalcularAtaque());
-            
-
-        }
-        if (other.CompareTag("Enemy3"))
-        {
-            MaestraNegra.Instance.RecibirDaño(StatsPlayer.Instance.CalcularAtaque());
-        }*/
-
-        //*********************************
         if (other.CompareTag("Enemy"))
         {
             other.GetComponent<movimientoEnemigo>().ReceivedDamage(StatsPlayer.Instance.CalcularAtaque());
@@ -86,8 +54,11 @@ public class Bala : MonoBehaviour
 
             JefaLilith.Instance.RecibirDaño(StatsPlayer.Instance.CalcularAtaque());
             Destroy(gameObject);
-
         }
+    }
 
+    private void OnDestroy()
+    {
+        Instantiate(ParticulaExplocion, transform.position, Quaternion.identity);
     }
 }
