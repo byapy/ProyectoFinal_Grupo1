4a4fbc15083923a8c7644b6c778735e4de36e6c6
PatitoDuck <patitinas@gmail.com>
2025-06-03 17:53:07 -0600

    AsigneGameover

--- Assets/UIFinal/AnimacionesGameOver.cs
+++ Assets/UIFinal/AnimacionesGameOver.cs
@@ -13,11 +13,16 @@ public class AnimacionesGameOver : MonoBehaviour
 
         LeanTween.scale(textoHasM.gameObject, new Vector3(1f, 1f, 1f), 0.5f).setDelay(1f).setEase(LeanTweenType.easeInOutBack);
         
-        textoHasM.LeanAlpha(1, 1f).setDelay(1.5f);//le da opacidad
+        textoHasM.LeanAlpha(1, 1f).setDelay(1.5f).setOnComplete(DetenerTodo);
+        
+        //le da opacidad
 
         // LeanTween.moveLocal(PanelNegro, new Vector3(0f, 0f, 0f), 0.5f).setDelay(1f).setEase(LeanTweenType.easeInOutBack);//este lo mueve para arriba
 
         //Aqui estaba poniendo las animaciones 
     }
-
+    void DetenerTodo()
+    {
+        Time.timeScale = 0f;
+    }
 }
